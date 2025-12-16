22f34fbe863e561d0e7c5ff93e392176e45c79e8
mobashirahmad569-cyber <mobashirahmad569@gmail.com>
2025-12-16 13:21:39 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -132,7 +132,7 @@ TELEGRAM_DOWNLOAD_EDIT_SLEEP = int(getenv("TELEGRAM_EDIT_SLEEP", "5"))
 
 
 # Your Github Repo.. Will be shown on /start Command
-GITHUB_REPO = getenv("GITHUB_REPO", "https://github.com/venombolteop/VenomMusic")
+GITHUB_REPO = getenv("GITHUB_REPO", "https://github.com/mobashirahmad569-cyber/VenomMusic")
 
 
 # Spotify Client.. Get it from https://developer.spotify.com/dashboard
